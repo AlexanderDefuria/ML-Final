fa4b017ad1daf141b6cef9a25557e779f79300b7
AlexanderDeFuria <defuria.alexander@gmail.com>
2024-04-14 11:35:50 -0400

    final

--- main.py
+++ main.py
@@ -11,26 +11,36 @@ class Test:
         self.part_one_trivial_core = [9, 10, 11, 23, 27]
         self.part_two_trivial_core = [28, 30, 37, 38, 40]
         self.part_three_trivial_core = [43, 45, 46, 49, 50, 51]
-        self.part_one_proof_core = []
-        self.part_two_proof_core = [31, 39, 41, 42]
-        self.part_three_proof_core = [47]
         self.part_one_trivial = list(range(1, 28))
         self.part_two_trivial = [28, 29, 30, 32, 34, 37, 38, 40]
         self.part_three_trivial = [43, 44, 45, 46, 48, 49, 50, 51]
+        
+        self.part_one_proof_core = []
+        self.part_two_proof_core = [31, 39, 41, 42]
+        self.part_three_proof_core = [47]
         self.part_one_proof = []
         self.part_two_proof = [31, 33, 35, 36, 39, 41, 42]
         self.part_three_proof = [47]
 
     def pick_proofs(self):
-        sections = [self.part_two_proof, self.part_three_proof, self.part_two_proof]
+        sections = [
+            (self.part_two_proof, self.part_two_proof_core),
+            (self.part_two_proof, self.part_two_proof_core),
+            (self.part_three_proof, self.part_three_proof_core),
+        ]
         questions = []
         for part in sections:
-            if random.choice([True, False]):
-                questions.append(random.choice(part))
-                part.remove(questions[-1])
-            else:
-                questions.append(random.choice(part))
-                part.remove(questions[-1])
+            while True:
+                try:
+                    group = random.choice(part)
+                    choice = random.choice(group)
+                    if choice not in questions:
+                        questions.append(choice)
+                        break
+                except:
+                    pass
+            
+        assert len(questions) == 3
         return questions
 
     def pick_trivials(self):
@@ -51,14 +61,33 @@ class Test:
                 except:
                     pass
 
+        assert len(questions) == 9
         return questions
     
 def plot():
     
     results = np.zeros(52)
-    for i in range(1000000):
-        test = Test()
-        questions = test.pick_proofs() + test.pick_trivials()
+    origin = Test()
+    samples = 10000000
+    for i in range(samples):
+        acceptable = False
+        while not acceptable:
+            test = Test()
+            questions = test.pick_proofs() + test.pick_trivials()
+            
+            # Check how many questions are core
+            core = 0
+            for question in questions:
+                if question in origin.part_one_trivial_core + origin.part_two_trivial_core + origin.part_three_trivial_core:
+                    core += 1
+                if question in origin.part_one_proof_core + origin.part_two_proof_core + origin.part_three_proof_core:
+                    core += 1
+            
+            # Check if there are at least half core questions
+            if core >= 6:
+                acceptable = True
+        
+        
         for question in questions:
             results[question] += 1
             
@@ -94,13 +123,13 @@ def plot():
     # Plot
     plt.figure(figsize=(16, 6))
     sns.barplot(x=df.index, y=df[0], hue=df["Label"])
-    plt.title("Question Frequency")
+    plt.title(f"Question Frequency ({samples} samples)")
     plt.xlabel("Question")
     plt.ylabel("Frequency")
     plt.savefig("question_frequency.png")
     
     
 if __name__ == "__main__":
-
-    print(Test().pick_proofs())
-    print(Test().pick_trivials())
+    plot()
+    # print(Test().pick_proofs())
+    # print(Test().pick_trivials())
